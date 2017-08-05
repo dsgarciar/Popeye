4fd85f41c0135326008481eab9119e7e6880cfef
Diego Garcia <diego@dante.rucaco.com>
2017-08-04 23:15:43 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <strings.h>
+
+int main (int argc, char *argv[]) {
+
+	char i;
+	getc (i);
+
+	sprintf(stdout, "Hello world\n");
+}
+
---
+++ main.cpp
@@ -0,0 +1,45 @@
+# include <iostream>
+
+
+int
+main (int argc, char *argv[]) {
+
+int i, o;
+std::cout << "Ingrese un numero: ";
+std::cin >> i;
+std::cout << "Hello world, el numero es: " << i << std::endl;
+
+
+
+
+return (0);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
